ae8e46caad7320cbb9b4a761d7b972e3e71af4f2
trena <shamlima.trena@northsouth.edu>
2025-03-22 13:59:40 +0600

    created another page for better layout design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
 import List from "./pages/List";
+import MedList from "./pages/MedList";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/list" element={<List />} />
+          <Route path="/med-list" element={<MedList />} />
         </Routes>
       </Router>
     </>
