f1d17c63e12d60df6253a2372ea2545625335220
trena <shamlima.trena@northsouth.edu>
2025-03-24 00:02:40 +0600

    update route for LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import List from "./pages/List";
 import AdminList from "./pages/AdminList";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/list" element={<List />} />
           <Route path="/admin-list" element={<AdminList />} />
+          <Route path="/landing-page" element={<LandingPage />} />
         </Routes>
       </Router>
     </>
