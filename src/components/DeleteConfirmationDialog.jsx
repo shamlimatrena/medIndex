3c6869dcf7eafc2fe9d359786197202e76ec99ae
trena <shamlima.trena@northsouth.edu>
2025-03-23 15:52:51 +0600

    add DeleteConfirmationDialog component

---
+++ src/components/DeleteConfirmationDialog.jsx
@@ -0,0 +1,47 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Typography,
+  } from "@mui/material";
+  
+  const DeleteConfirmationDialog = ({
+    open,
+    handleDeleteCancel,
+    handleDeleteMedicine,
+  }) => {
+    return (
+      <Dialog
+        open={open}
+        onClose={handleDeleteCancel}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Confirm Delete"}</DialogTitle>
+        <DialogContent>
+          <Typography id="alert-dialog-description">
+            Are you sure you want to delete this medicine? This action cannot be
+            undone.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteCancel} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={handleDeleteMedicine}
+            color="error"
+            variant="contained"
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+  
+  export default DeleteConfirmationDialog;
+  
