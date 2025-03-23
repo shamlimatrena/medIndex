4207b9da16ec2b86709c0093f15aff20db70431a
trena <shamlima.trena@northsouth.edu>
2025-03-24 05:49:50 +0600

    integrate API to List

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -3,9 +3,13 @@ import React, { useState, useEffect } from "react";
 //Component section
 import Header from "../components/Header";
 import MedicineList from "../components/MedicineList";
+import axios from "axios";
 
 //Dummy Data
 import Data from "../data/med.json";
+
+// API Route
+const API_URL = import.meta.env.VITE_API_URL;
 
 const MedicineIndexApp = () => {
   const [medicines, setMedicines] = useState([]);
@@ -13,10 +17,19 @@ const MedicineIndexApp = () => {
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    const storedMedicines =
-      JSON.parse(localStorage.getItem("medicines")) || Data;
-    setMedicines(storedMedicines);
-    localStorage.setItem("medicines", JSON.stringify(storedMedicines));
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setMedicines(response.data);
+        localStorage.setItem("medicines", JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.error("Error fetching medicines:", error);
+
+        const storedMedicines =
+          JSON.parse(localStorage.getItem("medicines")) || [];
+        setMedicines(storedMedicines);
+      });
   }, []);
 
   const filteredMedicines = medicines.filter(
