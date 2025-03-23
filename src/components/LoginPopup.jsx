a3e051ce3245fca1179c666e192fa5a5c8a78dce
trena <shamlima.trena@northsouth.edu>
2025-03-24 03:54:32 +0600

    create LoginPopup component

---
+++ src/components/LoginPopup.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const LoginPopup = ({ openLogin, handleCloseLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+  
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Handle login logic here
+    console.log("Login attempted with:", username, password);
+
+    //place holder
+    if (username == "trena" && password == "1234") {
+      navigate("/admin-list");
+      handleCloseLogin();
+    } else {
+      setLoginError(true);
+    }
+  };
+
+  const handleInputChange = (e, setter) => {
+    setLoginError(false);
+    setter(e.target.value);
+  };
+
+  return (
+    <Dialog open={openLogin} onClose={handleCloseLogin}>
+      <DialogTitle sx={{ bgcolor: "primary.light", color: "white" }}>
+        Admin Login
+      </DialogTitle>
+      <DialogContent sx={{ pt: 2, pb: 1, px: 3, mt: 2 }}>
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Username"
+          type="text"
+          fullWidth
+          variant="outlined"
+          value={username}
+          onChange={(e) => handleInputChange(e, setUsername)}
+          sx={{ mb: 2 }}
+        />
+        <TextField
+          margin="dense"
+          label="Password"
+          type="password"
+          fullWidth
+          variant="outlined"
+          value={password}
+          onChange={(e) => handleInputChange(e, setPassword)}
+          error={loginError}
+          helperText={loginError ? "Invalid username or password" : ""}
+        />
+      </DialogContent>
+      <DialogActions sx={{ px: 3, pb: 3 }}>
+        <Button onClick={handleCloseLogin} variant="text">
+          Cancel
+        </Button>
+        <Button onClick={handleLogin} variant="contained">
+          Login
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+export default LoginPopup;
